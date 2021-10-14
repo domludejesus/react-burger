2340e97a9e2d6ceb171ceeda8a29235b5f299a05
Dominic DeJesus <domludejesus@gmail.com>
2021-10-13 20:15:13 -0400

    'Hero Section'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import Navbar from './components/Navbar'; 
 import {BrowserRouter as Router} from 'react-router-dom'; 
 import { GlobalStyle } from "./globalStyles";
+import Hero from "./components/Hero";
+
 
 
 function App() {
   return (
     <Router>
-      <GlobalStyle/>
-      <Navbar />     
+      <GlobalStyle />
+      <Hero />    
     </Router>
   );
 }

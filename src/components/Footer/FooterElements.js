b392338433f52623959ec0c5289d6075580ab39b
Dominic DeJesus <domludejesus@gmail.com>
2021-10-14 18:29:33 -0400

    'Featured componet'

---
+++ src/components/Footer/FooterElements.js
@@ -0,0 +1,13 @@
+import styled from 'styled-componets'; 
+import { Link } from 'react-router-dom'; 
+
+export const FooterContainer = styled.footer`
+    background-color: #0d0909; 
+`; 
+
+export const FooterWrap = styled.div`
+    padding: 16px 24px; 
+    display: flex; 
+    flex-direction: column; 
+    justify-content: 
+`
